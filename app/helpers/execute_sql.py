4589cd36a895fb14798eb12bc2cf06f90d3b0ce6
berkayklnc <kilincb21@itu.edu.tr>
2024-11-03 03:21:11 +0300

    fix bugs

--- app/helpers/execute_sql.py
+++ app/helpers/execute_sql.py
@@ -5,23 +5,19 @@ def execute_sql_file(filename):
 
     with open(filename, 'r') as file:
         sql_statements = file.read()
-        print(sql_statements)
         cur = mysql.connection.cursor()
-        print(cur)
-        for statement in sql_statements.split(';'):
-            print(statement)
         try:
             for statement in sql_statements.split(';'):
                 if statement.strip():
                     cur.execute(statement.strip())
-            mysql.connection.commit()  # Değişiklikleri kaydet
+            mysql.connection.commit()
         except Exception as e:
-            mysql.connection.rollback()  # Hata durumunda geri al
+            mysql.connection.rollback()
             print(f"Error executing SQL statements: {e}")
         finally:
-            cur.close()  # Cursor'ı kapat
+            cur.close()
 def setup_database():
     with current_app.app_context():
-        execute_sql_file('sql/create_database.sql')
-        execute_sql_file('sql/create_users_table.sql')
-        execute_sql_file('sql/create_planes_table.sql')
+        execute_sql_file('app/sql/create_database.sql')
+        execute_sql_file('app/sql/create_users_table.sql')
+        execute_sql_file('app/sql/create_planes_table.sql')
