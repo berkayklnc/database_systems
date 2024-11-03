25f46aa26fd7610bacd08a9dc214dfa8c119def6
Çınar Vural <cinarvural@CnarDizlgisayar.mshome.net>
2024-11-03 13:50:16 +0300

    create players table

---
+++ app/sql/create_players_table.sql
@@ -0,0 +1,15 @@
+USE db_systems;
+CREATE TABLE IF NOT EXISTS players(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    balance INT,
+    user_id INT UNSIGNED NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    user_name VARCHAR(15) NOT NULL,
+    game_mode_id ENUM('EASY','MEDIUM','HARD') NOT NULL,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    CONSTRAINT fk_game_mode_id FOREIGN KEY (game_mode_id) REFERENCES game_mode(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
