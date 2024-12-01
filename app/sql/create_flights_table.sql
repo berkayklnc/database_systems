414ded7eaea7ff59c63dc44cf6c7b68abd70b7ba
berkayklnc <kilincb21@itu.edu.tr>
2024-12-01 17:56:39 +0300

    Fix in flight table

--- app/sql/create_flights_table.sql
+++ app/sql/create_flights_table.sql
@@ -4,10 +4,15 @@ CREATE TABLE IF NOT EXISTS flights (
     dest_city VARCHAR(20) NOT NULL,
     flight_time datetime NOT NULL,
     travel_time INT,
-    plane_id INT,
-    player_id INT,
+    plane_id INT UNSIGNED,
+    player_id INT UNSIGNED,
     economy_ticket_price FLOAT(6,2),
     business_ticket_price FLOAT(6,2),
-    FOREIGN KEY (player_id) REFERENCES players(id),
-    FOREIGN KEY (plane_id) REFERENCES planes(id),
+    FOREIGN KEY (player_id) REFERENCES players(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+
+    FOREIGN KEY (plane_id) REFERENCES planes(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
